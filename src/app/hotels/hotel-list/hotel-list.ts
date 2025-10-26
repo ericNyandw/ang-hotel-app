bdea6fd6708e058244fddee3639ff8c1cff2dced
nyandwi <eric.nyandwi.didier@gmail.com>
2025-10-26 20:50:55 +0100

    Another way to implement the Hotel component

--- src/app/hotels/hotel-list/hotel-list.ts
+++ src/app/hotels/hotel-list/hotel-list.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {DataService} from '../../services/data-service';
+
 
 import {HotelService} from '../../services/hotel-service';
-import {Hotel} from '../../shared/models/hotel';
 import {catchError, map, Observable, of} from 'rxjs';
+import {AppState} from '../../shared/models/app-state';
 
 @Component({
   selector: 'app-hotel-list',
@@ -13,24 +13,13 @@ import {catchError, map, Observable, of} from 'rxjs';
 })
 export class HotelList implements OnInit {
   public  myTiltet:string = 'List of Hotels ';
-
-  hotels$!: Observable<Hotel[] | null>; // Observable pour les hôtels
-  error: any;
-  loading = true; // Ajout d'une variable pour l'état de chargement
-  constructor(private readonly dataService: DataService, private readonly hotelService:HotelService) {}
-
+  appState$!:Observable<AppState>;
+  constructor(private readonly hotelService:HotelService) {}
 
   ngOnInit(): void {
-    this.hotels$ = this.hotelService.getHotels().pipe(
-      map(hotels => {
-        this.loading = false; // Arrête l'état de chargement en cas de succès
-        return hotels;
-      }),
-      catchError(error => {
-        this.loading = false; // Arrête l'état de chargement en cas d'erreur
-        this.error = error; // Stocke l'erreur pour affichage
-        return of(null); // Retourne un observable de null pour continuer le flux
-      })
+    this.appState$ = this.hotelService.getHotels().pipe(
+      map(hotels => ({hotels, loading: false, error: null})),
+      catchError(error => of({hotels: null, loading: false, error}))
     );
   }
 
